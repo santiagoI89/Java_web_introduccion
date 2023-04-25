d4444764ad6e7e90a67716b44cfa25040fc44ca3
santiagoI89 <104775984+santiagoI89@users.noreply.github.com>
2023-04-25 15:43:08 -0500

    Update ejecutar.java

--- figurasmaven/figurasgeometricas/src/main/java/figuras/ejecutar.java
+++ figurasmaven/figurasgeometricas/src/main/java/figuras/ejecutar.java
@@ -1,57 +1,40 @@
 package figuras;
 import java.util.*;
 public class ejecutar 
-{
-    public static void main(String[] args) 
-    { 
-        Scanner ingreso = new Scanner(System.in);
-
+{public static void main(String[] args) 
+    { Scanner ingreso = new Scanner(System.in);
         triangulo mytriangulo = new triangulo(0, 0, 0, 0);
         trapecio myTrapecio = new trapecio(0, 0, 0, 0, 0);
         decagono myDecagono = new decagono(0, 0, 0, 0);
-
         int seguir=0,seleccion;
-
-        do{
-            System.out.println("Calcular perimetro y area");
+        do{System.out.println("Calcular perimetro y area");
             System.out.println("Escoja una figura\nDigite 1 para triangulo\nDigite 2 para trapecio\nDigite 3 para decagono ");
             seleccion=ingreso.nextInt();
             switch(seleccion)
-            {
-                case 1:
-                {                   
-                    System.out.println("Calcular perimetro:");
+            {case 1:{System.out.println("Calcular perimetro:");
                     mytriangulo.calcularPerimetro();
                     System.out.println("Calcular area");
                     mytriangulo.calcularArea();
-                    break;
-                }
-                case 2:
-                {
+                    break;}
+                case 2:{System.out.println("Trapecio");
                     System.out.println("Calcular perimetro:");
                     myTrapecio.calcularPerimetro();
                     System.out.println("Calcular area");
                     myTrapecio.calcularArea();
-                    break;
-                }
-                case 3:
-                {
+                    break;}
+                case 3:{
                     System.out.println("Calcular perimetro:");
                     myDecagono.calcularPerimetro();
                     System.out.println("Calcular area");
                     myDecagono.calcularArea();
-                    break;
-                }
+                    System.out.println("fin");
+                    break;               }
                 default:
-                {
-                    System.out.println("Ingreso incorrecto");
-                    break;
-                }
+                { System.out.println("Ingreso incorrecto");
+                    break;}
             }
           System.out.println("Digte 1 para continuar o cualquier otro numero para salir");
-          seguir=ingreso.nextInt();
-          
+          seguir=ingreso.nextInt();       
         }while(seguir==1);
         ingreso.close();
-    }   
-}
+    }  }
